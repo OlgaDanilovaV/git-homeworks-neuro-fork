11acec9e37496adb39d062b3c4a39bd717cc73b9
Oleg <oleg@localhost>
2020-04-29 22:55:06 +0300

    Changed js/app

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
 (async () => {
-<<<<<<< HEAD
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            const coordsEl = document.querySelector('#user-coords');
-            coordsEl.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
-            coordsEl.parentElement.classList.remove('invisible');
-        });
-    }
-=======
     // TODO: add custom logic here
->>>>>>> d11238dcbc9be39513620e54e31387ad441e0205
 })();
